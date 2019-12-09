06257f4e9f2917a3028dacd13f2768f5a83bda10
deadnettle90 <49046656+deadnettle90@users.noreply.github.com>
2019-12-08 18:56:18 -0700

    added the find by country method and tests (#4)

--- config/environment.rb
+++ config/environment.rb
@@ -2,5 +2,5 @@ require 'bundler/setup'
 Bundler.require
 
 require_all 'lib'
-
+ActiveRecord::Base.logger = nil
 MainMenu.init
--- test/festival_services_spec.rb
+++ test/festival_services_spec.rb
@@ -18,10 +18,10 @@ RSpec.describe FestivalService do
         #arrange
             festival_name = "Boom" 
             festival = instance_double("festival", :name => festival_name)
-            festival_repo = double
-            allow(festival_repo).to receive(:find_by) {festival}
+            fest_repo = double
+            allow(fest_repo).to receive(:find_by) {festival}
         #act
-            service = FestivalService.new(festival_repo)
+            service = FestivalService.new(fest_repo)
             result = service.find_by_name(festival_name)
         #assert
             result.name == festival_name
@@ -32,5 +32,32 @@ RSpec.describe FestivalService do
             service = FestivalService.new(nil)
             expect{service.find_by_name(nil)}.to raise_error("name cannot be nil")
         end
+    end
+    
+    describe "#find_by_country" do
+        it "should return empty list if nothing found" do
+            fest_repo = double
+            allow(fest_repo).to receive(:where) {[]}
+
+            service = FestivalService.new(fest_repo)
+            result = service.find_by_country("USA")
+            result.size == 0
+        end
+        it "should return a list of all festivals that match a country" do
+         #arrange   
+            festival_country = "Portugal"
+            festival = instance_double("festival", :country => festival_country)
+            fest_repo = double
+            allow(fest_repo).to receive(:where) {[festival]}
+        #act 
+            service = FestivalService.new(fest_repo)
+            result = service.find_by_country(festival_country)
+        #assert
+            result[0].country == festival_country
+        end
+        it "should raise an exception when passed nil" do
+            service = FestivalService.new(nil)
+            expect{service.find_by_country(nil)}.to raise_error("country cannot be nil")
+        end      
     end
 end
